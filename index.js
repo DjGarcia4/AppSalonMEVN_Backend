0e16056803184533065a3d11b518ac42cfe4d3af
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-03-21 20:54:37 -0600

    CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import cors from "cors";
 import { db } from "./config/db.js";
 import servicesRoutes from "./routes/servicesRoutes.js";
 
@@ -12,6 +13,20 @@ const app = express();
 app.use(express.json());
 //Connect to db
 db();
+//Config CORS
+const whiteList = [process.env.FRONTEND_URL, undefined];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) {
+      //Allow  the request
+      callback(null, true);
+    } else {
+      //Do not  allow the request
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 //Define Routes
 app.use("/api/services", servicesRoutes);
 //Define Port
