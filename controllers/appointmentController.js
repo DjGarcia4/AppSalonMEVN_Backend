db26b007baffc16ee7d2ee62f24d395845cb8eae
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-04-01 14:34:08 -0600

    validate hours availables

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,4 +1,5 @@
 import Appointment from "../models/Appointmen.js";
+import { parse, formatISO, startOfDay, endOfDay, isValid } from "date-fns";
 
 const createAppointment = async (req, res) => {
   const appointment = req.body;
@@ -10,8 +11,33 @@ const createAppointment = async (req, res) => {
       msg: "Tu reservacion se realizo correctamente!",
     });
   } catch (error) {
-    console.log();
+    console.log(error);
   }
 };
 
-export { createAppointment };
+const getAppointmentById = async (req, res) => {
+  const { date } = req.query;
+  const newDate = parse(date, "dd/MM/yyyy", new Date());
+
+  if (!isValid(newDate)) {
+    const error = new Error("Fecha no valida!");
+    return res.status(400).json({
+      msg: error.message,
+    });
+  }
+
+  const isoDate = formatISO(newDate);
+  try {
+    const appointments = await Appointment.find({
+      date: {
+        $gte: startOfDay(new Date(isoDate)),
+        $lte: endOfDay(new Date(isoDate)),
+      },
+    }).select("time");
+    res.json(appointments);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { createAppointment, getAppointmentById };
